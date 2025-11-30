581cbe75ba5da7b4376c2b82222c690f87d07789
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 22:45:25 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/addiw.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/addiw.d
@@ -0,0 +1,5 @@
+addiw.o: addiw.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/addiw.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/addiw.h:
