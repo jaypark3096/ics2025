67300325ac7e0d18238b440b12e011fac86d5e18
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 21:13:22 +0900

    Upload file: icsold/am-kernels/tests/cpu-tests/build/riscv32-nemu/tests/div.d

---
+++ icsold/am-kernels/tests/cpu-tests/build/riscv32-nemu/tests/div.d
@@ -0,0 +1,8 @@
+/home/hook/ics2025/am-kernels/tests/cpu-tests/build/riscv32-nemu/tests/div.o: \
+ /home/hook/ics2025/am-kernels/tests/cpu-tests/tests/div.c \
+ /home/hook/ics2025/am-kernels/tests/cpu-tests/include/trap.h \
+ /home/hook/ics2025/abstract-machine/am/include/am.h \
+ /home/hook/ics2025/abstract-machine/am/include/arch/riscv.h \
+ /home/hook/ics2025/abstract-machine/am/include/amdev.h \
+ /home/hook/ics2025/abstract-machine/klib/include/klib.h \
+ /home/hook/ics2025/abstract-machine/klib/include/klib-macros.h
