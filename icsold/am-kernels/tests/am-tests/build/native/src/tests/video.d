2a2783b0f6fa29aa6011f396faf45e3e22907591
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 21:09:34 +0900

    Upload file: icsold/am-kernels/tests/am-tests/build/native/src/tests/video.d

---
+++ icsold/am-kernels/tests/am-tests/build/native/src/tests/video.d
@@ -0,0 +1,8 @@
+/home/hook/ics2025/am-kernels/tests/am-tests/build/native/src/tests/video.o: \
+ /home/hook/ics2025/am-kernels/tests/am-tests/src/tests/video.c \
+ /home/hook/ics2025/am-kernels/tests/am-tests/include/amtest.h \
+ /home/hook/ics2025/abstract-machine/am/include/am.h \
+ /home/hook/ics2025/abstract-machine/am/include/arch/native.h \
+ /home/hook/ics2025/abstract-machine/am/include/amdev.h \
+ /home/hook/ics2025/abstract-machine/klib/include/klib.h \
+ /home/hook/ics2025/abstract-machine/klib/include/klib-macros.h
