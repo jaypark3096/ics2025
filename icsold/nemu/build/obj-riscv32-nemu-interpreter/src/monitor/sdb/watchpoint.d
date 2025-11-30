be323822828619cb41024d63843a6a11cabb0df2
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 21:28:44 +0900

    Upload file: icsold/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/watchpoint.d

---
+++ icsold/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/watchpoint.d
@@ -0,0 +1,19 @@
+cmd_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/watchpoint.o := unused
+
+source_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/watchpoint.o := src/monitor/sdb/watchpoint.c
+
+deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/watchpoint.o := \
+  src/monitor/sdb/sdb.h \
+  /home/hook/ics2025/nemu/include/common.h \
+    $(wildcard include/config/target/am.h) \
+    $(wildcard include/config/mbase.h) \
+    $(wildcard include/config/msize.h) \
+    $(wildcard include/config/isa64.h) \
+  /home/hook/ics2025/nemu/include/macro.h \
+  /home/hook/ics2025/nemu/include/debug.h \
+  /home/hook/ics2025/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
+
+/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/watchpoint.o: $(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/watchpoint.o)
+
+$(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/monitor/sdb/watchpoint.o):
