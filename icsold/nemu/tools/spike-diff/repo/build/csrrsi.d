b31dc3bdb97d1ecd571c4d38ad9af7659a421ef1
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 22:47:55 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/csrrsi.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/csrrsi.d
@@ -0,0 +1,5 @@
+csrrsi.o: csrrsi.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/csrrsi.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/csrrsi.h:
