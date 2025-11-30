53b9dc436c6b53e0aaca81dda3b47a6f05eb9bbd
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 21:27:47 +0900

    Upload file: icsold/nemu/build/obj-riscv32-nemu-interpreter/src/device/alarm.d

---
+++ icsold/nemu/build/obj-riscv32-nemu-interpreter/src/device/alarm.d
@@ -0,0 +1,19 @@
+cmd_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/device/alarm.o := unused
+
+source_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/device/alarm.o := src/device/alarm.c
+
+deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/device/alarm.o := \
+  /home/hook/ics2025/nemu/include/common.h \
+    $(wildcard include/config/target/am.h) \
+    $(wildcard include/config/mbase.h) \
+    $(wildcard include/config/msize.h) \
+    $(wildcard include/config/isa64.h) \
+  /home/hook/ics2025/nemu/include/macro.h \
+  /home/hook/ics2025/nemu/include/debug.h \
+  /home/hook/ics2025/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
+  /home/hook/ics2025/nemu/include/device/alarm.h \
+
+/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/device/alarm.o: $(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/device/alarm.o)
+
+$(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/device/alarm.o):
