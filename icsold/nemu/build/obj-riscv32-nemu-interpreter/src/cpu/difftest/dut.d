422cd76a4bca6065b6710f12db45fc879f3665d5
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 21:27:43 +0900

    Upload file: icsold/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/dut.d

---
+++ icsold/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/dut.d
@@ -0,0 +1,31 @@
+cmd_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/dut.o := unused
+
+source_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/dut.o := src/cpu/difftest/dut.c
+
+deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/dut.o := \
+    $(wildcard include/config/difftest.h) \
+  /home/hook/ics2025/nemu/include/isa.h \
+  /home/hook/ics2025/nemu/src/isa/riscv32/include/isa-def.h \
+    $(wildcard include/config/rve.h) \
+    $(wildcard include/config/rv64.h) \
+  /home/hook/ics2025/nemu/include/common.h \
+    $(wildcard include/config/target/am.h) \
+    $(wildcard include/config/mbase.h) \
+    $(wildcard include/config/msize.h) \
+    $(wildcard include/config/isa64.h) \
+  /home/hook/ics2025/nemu/include/macro.h \
+  /home/hook/ics2025/nemu/include/debug.h \
+  /home/hook/ics2025/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
+  /home/hook/ics2025/nemu/include/cpu/cpu.h \
+  /home/hook/ics2025/nemu/include/memory/paddr.h \
+    $(wildcard include/config/pc/reset/offset.h) \
+  /home/hook/ics2025/nemu/include/difftest-def.h \
+    $(wildcard include/config/isa/x86.h) \
+    $(wildcard include/config/isa/mips32.h) \
+    $(wildcard include/config/isa/riscv.h) \
+    $(wildcard include/config/isa/loongarch32r.h) \
+
+/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/dut.o: $(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/dut.o)
+
+$(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/cpu/difftest/dut.o):
