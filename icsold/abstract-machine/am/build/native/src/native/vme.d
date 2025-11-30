77094669e499f466b999d51406cc9d35dacb6b31
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 20:44:31 +0900

    Upload file: icsold/abstract-machine/am/build/native/src/native/vme.d

---
+++ icsold/abstract-machine/am/build/native/src/native/vme.d
@@ -0,0 +1,8 @@
+/home/hook/ics2025/abstract-machine/am/build/native/src/native/vme.o: \
+ /home/hook/ics2025/abstract-machine/am/src/native/vme.c \
+ /home/hook/ics2025/abstract-machine/am/src/native/platform.h \
+ /home/hook/ics2025/abstract-machine/am/include/am.h \
+ /home/hook/ics2025/abstract-machine/am/include/arch/native.h \
+ /home/hook/ics2025/abstract-machine/am/include/amdev.h \
+ /home/hook/ics2025/abstract-machine/klib/include/klib.h \
+ /home/hook/ics2025/abstract-machine/klib/include/klib-macros.h
