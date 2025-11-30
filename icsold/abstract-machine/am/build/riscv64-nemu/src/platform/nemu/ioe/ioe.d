f9a037e31043a8442b2610cf4b8b2e2c69fd0147
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 20:45:24 +0900

    Upload file: icsold/abstract-machine/am/build/riscv64-nemu/src/platform/nemu/ioe/ioe.d

---
+++ icsold/abstract-machine/am/build/riscv64-nemu/src/platform/nemu/ioe/ioe.d
@@ -0,0 +1,6 @@
+/home/hook/ics2025/abstract-machine/am/build/riscv64-nemu/src/platform/nemu/ioe/ioe.o: \
+ /home/hook/ics2025/abstract-machine/am/src/platform/nemu/ioe/ioe.c \
+ /home/hook/ics2025/abstract-machine/am/include/am.h \
+ /home/hook/ics2025/abstract-machine/am/include/arch/riscv.h \
+ /home/hook/ics2025/abstract-machine/am/include/amdev.h \
+ /home/hook/ics2025/abstract-machine/klib/include/klib-macros.h
