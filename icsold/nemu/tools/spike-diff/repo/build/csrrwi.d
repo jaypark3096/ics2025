5d11b6d00f67efdb735879b39b46d55c556b75ca
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 22:48:03 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/csrrwi.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/csrrwi.d
@@ -0,0 +1,5 @@
+csrrwi.o: csrrwi.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/csrrwi.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/csrrwi.h:
