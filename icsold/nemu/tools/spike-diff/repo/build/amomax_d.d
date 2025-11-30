b4e8099a96bcb8c63df94f2619969a24afad86b8
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 22:46:00 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/amomax_d.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/amomax_d.d
@@ -0,0 +1,5 @@
+amomax_d.o: amomax_d.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/amomax_d.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/amomax_d.h:
