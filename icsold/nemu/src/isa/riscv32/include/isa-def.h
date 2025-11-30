6983eb2b73a37731c835a6e3ab1bc4a9d700018b
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-11-30 21:33:26 +0900

    Upload file: icsold/nemu/src/isa/riscv32/include/isa-def.h

---
+++ icsold/nemu/src/isa/riscv32/include/isa-def.h
@@ -0,0 +1,37 @@
+/***************************************************************************************
+* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#ifndef __ISA_RISCV_H__
+#define __ISA_RISCV_H__
+
+#include <common.h>
+
+typedef struct {
+	word_t mstatus;  // 添加这一行，用于存储机器状态寄存器mstatus
+	word_t mepc;    // 机器模式异常程序计数器，存储异常发生时的指令地址
+	word_t mcause;  // 机器模式异常原因寄存器，存储异常类型
+	word_t mtvec;   // 机器模式陷阱向量基地址寄存器，存储异常处理入口地址
+  word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
+  vaddr_t pc;
+} MUXDEF(CONFIG_RV64, riscv64_CPU_state, riscv32_CPU_state);
+
+// decode
+typedef struct {
+  uint32_t inst;
+} MUXDEF(CONFIG_RV64, riscv64_ISADecodeInfo, riscv32_ISADecodeInfo);
+
+#define isa_mmu_check(vaddr, len, type) (MMU_DIRECT)
+
+#endif
