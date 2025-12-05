bc54a83249260318501078cabc23bcc4935d0ea4
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:04:34 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/amoor_d.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/amoor_d.d
@@ -0,0 +1,5 @@
+amoor_d.o: amoor_d.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/amoor_d.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/amoor_d.h:
