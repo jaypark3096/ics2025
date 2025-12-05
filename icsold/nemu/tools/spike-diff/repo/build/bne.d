bb76a92ab622a5b82e9622c18aa6127da6fccf97
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:05:42 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/bne.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/bne.d
@@ -0,0 +1,5 @@
+bne.o: bne.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/bne.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/bne.h:
