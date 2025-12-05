3b5fc9a7fb7cbea2705f5597b30f97f6f550dfa8
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:03:39 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/amoadd_w.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/amoadd_w.d
@@ -0,0 +1,5 @@
+amoadd_w.o: amoadd_w.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/amoadd_w.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/amoadd_w.h:
