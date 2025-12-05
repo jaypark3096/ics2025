be112a5d1a86db87bfeae06e6e93bbe55e4eba8c
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:11:58 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/interactive.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/interactive.d
@@ -0,0 +1,87 @@
+interactive.o: \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/interactive.cc \
+ config.h /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/sim.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/cfg.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/decode.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../softfloat/softfloat_types.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/debug_module.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/abstract_device.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/devices.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/mmio_plugin.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/abstract_interrupt_controller.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/platform.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/log_file.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/processor.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/trap.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/encoding.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/debug_rom_defines.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/entropy_source.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/common.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/csrs.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/memtracer.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/isa_parser.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/triggers.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/memif.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/byteorder.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/../riscv/cfg.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/vector_unit.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/simif.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/htif.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/memif.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/syscall.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/device.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/byteorder.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/decode_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat_types.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/specialize.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/primitiveTypes.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/platform.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat_types.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/disasm.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/mmu.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/byteorder.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/socketif.h
+config.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/sim.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/cfg.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/decode.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../softfloat/softfloat_types.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/debug_module.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/abstract_device.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/devices.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/mmio_plugin.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/abstract_interrupt_controller.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/platform.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/log_file.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/processor.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/trap.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/encoding.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/debug_rom_defines.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/entropy_source.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/common.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/csrs.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/memtracer.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/isa_parser.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/triggers.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/memif.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/byteorder.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/../riscv/cfg.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/vector_unit.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/simif.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/htif.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/memif.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/syscall.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/device.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/byteorder.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/decode_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat_types.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/specialize.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/primitiveTypes.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/platform.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat_types.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/disasm.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/mmu.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/byteorder.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/socketif.h:
