f1facdabb7665678185097593cc4f578199cee11
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:08:36 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/c_lwsp.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/c_lwsp.d
@@ -0,0 +1,5 @@
+c_lwsp.o: c_lwsp.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/c_lwsp.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/c_lwsp.h:
