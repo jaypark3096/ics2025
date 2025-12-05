a170b7825e36a0fd15798571a52da4ab71083566
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:05:08 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/andi.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/andi.d
@@ -0,0 +1,5 @@
+andi.o: andi.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/andi.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/andi.h:
