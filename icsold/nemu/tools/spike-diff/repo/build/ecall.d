2fc5fda1fb0ef3d9c07ec4bebbd36ca7418d1b04
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:10:22 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/ecall.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/ecall.d
@@ -0,0 +1,5 @@
+ecall.o: ecall.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/ecall.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/ecall.h:
