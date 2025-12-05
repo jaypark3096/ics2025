69d4e7274c7d21c31ea3f28200f222ff0e91b1e5
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:10:34 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/execute.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/execute.d
@@ -0,0 +1,62 @@
+execute.o: /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/execute.cc \
+ config.h /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/processor.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/decode.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../softfloat/softfloat_types.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/trap.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/encoding.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/abstract_device.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/debug_rom_defines.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/entropy_source.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/common.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/csrs.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/memtracer.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/isa_parser.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/triggers.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/memif.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/byteorder.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/../riscv/cfg.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/../riscv/decode.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/vector_unit.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/mmu.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/simif.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/cfg.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/byteorder.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/disasm.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/decode_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat_types.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/specialize.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/primitiveTypes.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/platform.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat_types.h
+config.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/processor.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/decode.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../softfloat/softfloat_types.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/trap.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/encoding.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/abstract_device.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/debug_rom_defines.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/entropy_source.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/common.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/csrs.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/memtracer.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/isa_parser.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/triggers.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/memif.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/byteorder.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/../riscv/cfg.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/../riscv/decode.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/vector_unit.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/mmu.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/simif.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/cfg.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/../fesvr/byteorder.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/disasm.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/decode_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat_types.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/specialize.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/primitiveTypes.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/platform.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/softfloat/softfloat_types.h:
