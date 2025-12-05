ce4563252560c36182f6c448b18e24c60422e5e3
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:09:24 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/c_xor.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/c_xor.d
@@ -0,0 +1,5 @@
+c_xor.o: c_xor.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/c_xor.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/c_xor.h:
