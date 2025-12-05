0bac33f44868b65d76ec112b4163a5937196abef
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 18:12:01 +0900

    Upload file: icsold/abstract-machine/am/build/native/src/native/ioe/uart.d

---
+++ icsold/abstract-machine/am/build/native/src/native/ioe/uart.d
@@ -0,0 +1,5 @@
+/home/hook/ics2025/abstract-machine/am/build/native/src/native/ioe/uart.o: \
+ /home/hook/ics2025/abstract-machine/am/src/native/ioe/uart.c \
+ /home/hook/ics2025/abstract-machine/am/include/am.h \
+ /home/hook/ics2025/abstract-machine/am/include/arch/native.h \
+ /home/hook/ics2025/abstract-machine/am/include/amdev.h
