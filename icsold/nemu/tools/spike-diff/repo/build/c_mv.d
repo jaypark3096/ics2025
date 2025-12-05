db0d7c531ebdb1c37c8eeb5772b448814476a869
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:08:42 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/c_mv.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/c_mv.d
@@ -0,0 +1,5 @@
+c_mv.o: c_mv.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/c_mv.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/c_mv.h:
