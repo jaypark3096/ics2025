44398ddb61f43fb73bd7d43d8ecf1f97984c495e
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 18:16:57 +0900

    Upload file: icsold/abstract-machine/klib/build/native/src/string.d

---
+++ icsold/abstract-machine/klib/build/native/src/string.d
@@ -0,0 +1,7 @@
+/home/hook/ics2025/abstract-machine/klib/build/native/src/string.o: \
+ /home/hook/ics2025/abstract-machine/klib/src/string.c \
+ /home/hook/ics2025/abstract-machine/klib/include/klib.h \
+ /home/hook/ics2025/abstract-machine/am/include/am.h \
+ /home/hook/ics2025/abstract-machine/am/include/arch/native.h \
+ /home/hook/ics2025/abstract-machine/am/include/amdev.h \
+ /home/hook/ics2025/abstract-machine/klib/include/klib-macros.h
