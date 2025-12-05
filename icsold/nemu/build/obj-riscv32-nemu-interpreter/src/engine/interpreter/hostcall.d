25090c7ded6aa966cd2f30848f309759a8fd9ced
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 18:41:49 +0900

    Upload file: icsold/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.d

---
+++ icsold/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.d
@@ -0,0 +1,32 @@
+cmd_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o := unused
+
+source_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o := src/engine/interpreter/hostcall.c
+
+deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o := \
+    $(wildcard include/config/hostcall.h) \
+  /home/hook/ics2025/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
+  /home/hook/ics2025/nemu/include/common.h \
+    $(wildcard include/config/target/am.h) \
+    $(wildcard include/config/mbase.h) \
+    $(wildcard include/config/msize.h) \
+    $(wildcard include/config/isa64.h) \
+  /home/hook/ics2025/nemu/include/macro.h \
+  /home/hook/ics2025/nemu/include/debug.h \
+  /home/hook/ics2025/nemu/include/cpu/ifetch.h \
+  /home/hook/ics2025/nemu/include/memory/vaddr.h \
+  /home/hook/ics2025/nemu/include/isa.h \
+  /home/hook/ics2025/nemu/src/isa/riscv32/include/isa-def.h \
+    $(wildcard include/config/rve.h) \
+    $(wildcard include/config/rv64.h) \
+  /home/hook/ics2025/nemu/include/cpu/difftest.h \
+    $(wildcard include/config/difftest.h) \
+  /home/hook/ics2025/nemu/include/difftest-def.h \
+    $(wildcard include/config/isa/x86.h) \
+    $(wildcard include/config/isa/mips32.h) \
+    $(wildcard include/config/isa/riscv.h) \
+    $(wildcard include/config/isa/loongarch32r.h) \
+
+/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o: $(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o)
+
+$(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/engine/interpreter/hostcall.o):
