bc962b498616cf10bac3dc1f0385bd01725c2119
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:11:16 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/fence_i.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/fence_i.d
@@ -0,0 +1,5 @@
+fence_i.o: fence_i.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/fence_i.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/fence_i.h:
