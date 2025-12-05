4bec3664a39b4b2b6411af038f10a084e29d2f75
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 18:11:52 +0900

    Upload file: icsold/abstract-machine/am/build/native/src/native/ioe/disk.d

---
+++ icsold/abstract-machine/am/build/native/src/native/ioe/disk.d
@@ -0,0 +1,5 @@
+/home/hook/ics2025/abstract-machine/am/build/native/src/native/ioe/disk.o: \
+ /home/hook/ics2025/abstract-machine/am/src/native/ioe/disk.c \
+ /home/hook/ics2025/abstract-machine/am/include/am.h \
+ /home/hook/ics2025/abstract-machine/am/include/arch/native.h \
+ /home/hook/ics2025/abstract-machine/am/include/amdev.h
