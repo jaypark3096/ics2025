501ad57e581fa7303b3c68d88c9841c29c350360
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:04:49 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/amoswap_w.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/amoswap_w.d
@@ -0,0 +1,5 @@
+amoswap_w.o: amoswap_w.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/amoswap_w.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insn_macros.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/riscv/insns/amoswap_w.h:
