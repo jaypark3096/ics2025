d1f91fc5efbc179e36067cf8a65e049638f66483
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 18:16:50 +0900

    Upload file: icsold/abstract-machine/klib/build/native/src/int64.d

---
+++ icsold/abstract-machine/klib/build/native/src/int64.d
@@ -0,0 +1,5 @@
+/home/hook/ics2025/abstract-machine/klib/build/native/src/int64.o: \
+ /home/hook/ics2025/abstract-machine/klib/src/int64.c \
+ /home/hook/ics2025/abstract-machine/am/include/am.h \
+ /home/hook/ics2025/abstract-machine/am/include/arch/native.h \
+ /home/hook/ics2025/abstract-machine/am/include/amdev.h
