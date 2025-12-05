f0a16af091bd361b480cf05924d927aece2b35ab
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 18:42:09 +0900

    Upload file: icsold/nemu/build/obj-riscv32-nemu-interpreter/src/memory/paddr.d

---
+++ icsold/nemu/build/obj-riscv32-nemu-interpreter/src/memory/paddr.d
@@ -0,0 +1,34 @@
+cmd_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/memory/paddr.o := unused
+
+source_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/memory/paddr.o := src/memory/paddr.c
+
+deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/memory/paddr.o := \
+    $(wildcard include/config/mtrace.h) \
+    $(wildcard include/config/pmem/malloc.h) \
+    $(wildcard include/config/pmem/garray.h) \
+    $(wildcard include/config/msize.h) \
+    $(wildcard include/config/mbase.h) \
+    $(wildcard include/config/mtrace/cond.h) \
+    $(wildcard include/config/mem/random.h) \
+    $(wildcard include/config/device.h) \
+  /home/hook/ics2025/nemu/include/memory/host.h \
+    $(wildcard include/config/isa64.h) \
+    $(wildcard include/config/rt/check.h) \
+  /home/hook/ics2025/nemu/include/common.h \
+    $(wildcard include/config/target/am.h) \
+  /home/hook/ics2025/nemu/include/macro.h \
+  /home/hook/ics2025/nemu/include/debug.h \
+  /home/hook/ics2025/nemu/include/utils.h \
+    $(wildcard include/config/target/native/elf.h) \
+  /home/hook/ics2025/nemu/include/memory/paddr.h \
+    $(wildcard include/config/pc/reset/offset.h) \
+  /home/hook/ics2025/nemu/include/device/mmio.h \
+  /home/hook/ics2025/nemu/include/isa.h \
+  /home/hook/ics2025/nemu/src/isa/riscv32/include/isa-def.h \
+    $(wildcard include/config/rve.h) \
+    $(wildcard include/config/rv64.h) \
+  /home/hook/ics2025/nemu/include/cpu/cpu.h \
+
+/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/memory/paddr.o: $(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/memory/paddr.o)
+
+$(deps_/home/hook/ics2025/nemu/build/obj-riscv32-nemu-interpreter/src/memory/paddr.o):
