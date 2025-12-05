d0064b6762af5a1c895275db3b0af8c2038a5702
jaypark3096 <140645126+jaypark3096@users.noreply.github.com>
2025-12-05 20:09:28 +0900

    Upload file: icsold/nemu/tools/spike-diff/repo/build/device.d

---
+++ icsold/nemu/tools/spike-diff/repo/build/device.d
@@ -0,0 +1,16 @@
+device.o: /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/device.cc \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/device.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/term.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/memif.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/byteorder.h config.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/../riscv/cfg.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/../riscv/decode.h \
+ /home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/../riscv/../softfloat/softfloat_types.h
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/device.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/term.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/memif.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/byteorder.h:
+config.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/../riscv/cfg.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/../riscv/decode.h:
+/home/hook/ics2025/nemu/tools/spike-diff/repo/fesvr/../riscv/../softfloat/softfloat_types.h:
